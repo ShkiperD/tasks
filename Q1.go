6bfa37694972000267871ad888999a0f8e8a4e7a
ShkiperD <Dimashkip420@gmail.com>
2021-09-07 02:11:05 +0800

    Reload

--- Q1.go
+++ Q1.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
 func main() {
-	$END$
+	var Number = 42
+	var CheckingMin = 0
+	var CheckingMax = 100
+	var Checking int
+	fmt.Println("Загадайте число: ")
+	for{
+		Checking = Random(CheckingMin,CheckingMax)
+		break
+	}
+}
+func Random(checkMn, checkMx int) int {
+	rand.Seed(time.Now().UnixNano())
+	n:=rand.Intn(checkMn,checkMx)
+	return n
 }
--- SpaseTickets.go
+++ SpaseTickets.go
@@ -1,5 +1,46 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+)
+const way = 62100000
+const secOnDay = 86400
+
 func main() {
-	$END$
+	fmt.Printf("%-18v %-4v %-4v %6v\n", "Spaceline","Days","Trip type","Price")
+	fmt.Println("========================================")
+	for count:=0;count<10;count++{
+		speed,price:=daysOfAdv()
+		days:=way/(speed*secOnDay)
+		tripType:=tripTypeGen()
+		if tripType=="Round Trip"{price*=2}
+		fmt.Printf("%-19v %-3v %-12v %v\n",stationPick(),days,tripType,price)
+	}
+}
+func stationPick() string{
+	station:=rand.Intn(3)
+	var nameStation string
+	switch station {
+		case 0: nameStation = "SpaceX"
+		case 1:	nameStation = "VirginGalactic"
+		case 2: nameStation = "Space Adventure"
+	}
+	return nameStation
+}
+func daysOfAdv() (int,int) {
+	speedShip:= 16 + rand.Intn(14)
+	price:=36+(speedShip-16)
+	return speedShip,price
+}
+func tripTypeGen() string{
+	var tripType string
+	n:=rand.Intn(2)
+	switch n{
+	case 0:
+		tripType="Round Trip"
+	case 1:
+		tripType="One-way"
+	}
+	return tripType
 }
